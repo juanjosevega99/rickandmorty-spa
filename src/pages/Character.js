a0ed07081b3928d82a22bb3b94c66d8338961caf
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-06 23:14:54 -0500

    css para administrar elementos visuales

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -5,18 +5,18 @@ const Character = async () => {
   const id = getHash();
   const character = await getData(id);
   const view = `
-    <div className="Character-inner">
-      <article class="Character-card">
-        <img src="${character.image}" alt="${character.name}"/>
+    <div class="Characters-inner">
+      <article class="Characters-card">
+        <img src="${character.image}" alt="${character.name}">
         <h2>${character.name}</h2>
       </article>
-      <article className="Character-card">
+      <article class="Characters-card">
         <h3>Episodes: <span>${character.episode.length}</span></h3>
         <h3>Status: <span>${character.status}</span></h3>
         <h3>Species: <span>${character.species}</span></h3>
         <h3>Gender: <span>${character.gender}</span></h3>
         <h3>Origin: <span>${character.origin.name}</span></h3>
-        <h3>Las Location: ${character.location.name}</h3>
+        <h3>Last Location: ${character.location.name}</h3>
       </article>
     </div>
   `;
